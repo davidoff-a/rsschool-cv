289f6c736f6927222fdb3aff041a073943d58cea
Davydau Akiaksei <davidoff.aleksei@gmail.com>
2021-09-21 12:35:18 +0300

    fix: remove page scrolling in case opened menu, remove illegal character in path segment: space is not allowed

--- js/app.js
+++ js/app.js
@@ -6,32 +6,37 @@ const menu = document.querySelector(".layer");
 const menuLines = document.querySelectorAll(".menu__item");
 const backBtn = document.querySelector(".back");
 
-burger.addEventListener("click", ()=>{
+burger.addEventListener("click", () => {
   toggleMenu();
-})
+});
 
-menu.addEventListener("click", (event)=>{
-if (
-  event.target &&
-  (event.target.classList.contains("layer") ||
-    event.target.classList.contains("menu__link") ||
-    event.target.classList.contains("menu__item"))
-) {
-  toggleMenu();
-}});
+menu.addEventListener("click", (event) => {
+  if (
+    event.target &&
+    (event.target.classList.contains("layer") ||
+      event.target.classList.contains("menu__link") ||
+      event.target.classList.contains("menu__item"))
+  ) {
+    toggleMenu();
+  }
+});
 
-function toggleMenu(){
-    lines.forEach((line) => {
+function toggleMenu() {
+  lines.forEach((line) => {
     line.classList.toggle("close");
   });
   menu.classList.toggle("hide");
+  if (menu.classList.contains("hide")) {
+    document.body.style.overflow = "";
+  } else {
+    document.body.style.overflow = "hidden";
+  }
 }
 window.addEventListener("scroll", () => {
   let scrollTop = window.scrollY;
-  if (scrollTop>200){
+  if (scrollTop > 200) {
     backBtn.classList.remove("hide");
   } else {
     backBtn.classList.add("hide");
-
   }
 });
